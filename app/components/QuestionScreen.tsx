5031e3a743eec42cce11745598b401a71f69fbe0
Lukas Lücke <lukas@luecke.me>
2019-11-03 16:04:58 +0100

    Add language switch for questions

--- app/components/QuestionScreen.tsx
+++ app/components/QuestionScreen.tsx
@@ -1,20 +1,41 @@
 import * as Speech from 'expo-speech';
 
-import {Body, Button, Card, CardItem, Icon, Right, Text, View, Left} from 'native-base';
+import {Body, Button, Card, CardItem, Icon, Right, Text, View, Left, Picker} from 'native-base';
 import React, {useState} from "react";
 import {Alert, ScrollView} from "react-native";
 
-const questions = require('../assets/questions.json');
+const en = require(`../assets/questions_en.json`);
+const de = require(`../assets/questions_de.json`);
+const fr = require(`../assets/questions_fr.json`);
 
-type AssessmentProps = { indicator: string, questions: string[] }
+const questions = {
+    en,
+    de,
+    fr
+};
+
+const ui = {
+    changeLanguage: {
+        en: 'Change language',
+        de: 'Sprache wechseln',
+        fr: 'Changer de langue'
+    },
+    learnMore: {
+        en: 'Learn more',
+        de: 'Mehr erfahren',
+        fr: 'En savoir plus'
+    }
+}
+
+type AssessmentProps = { indicator: string, questions: string[], language: string }
 enum QuestionAnswer {
     YES,
     UNSURE,
     NO
 }
 
-const Question: React.FC<{question: string}> = props => {
-    const {question} = props;
+const Question: React.FC<{question: string, language: string}> = props => {
+    const {question, language} = props;
     const [selected, setSelected] = useState<QuestionAnswer>();
 
     const getSelectionProps = (value: QuestionAnswer) => ({
@@ -24,7 +45,7 @@ const Question: React.FC<{question: string}> = props => {
 
     return <Card style={{flex: 0, borderColor: 'ccc', borderWidth: 1}}><CardItem header>
         <Body><Text>{question}</Text></Body>
-        <Right><Button info onPress={() => Speech.speak(question)}><Icon name="volume-high"/></Button></Right>
+        <Right><Button info onPress={() => Speech.speak(question, {language})}><Icon name="volume-high"/></Button></Right>
     </CardItem>
         <CardItem>
             <View style={{flexDirection: "row", justifyContent: "space-between"}}>
@@ -38,27 +59,45 @@ const Question: React.FC<{question: string}> = props => {
 }
 
 const Assessment: React.FC<AssessmentProps> = props => {
+    const {language, questions} = props;
+
     return <Card style={{flex: 0, margin: 8}}>
         <CardItem header>
-            <Left><Text>{props.indicator}</Text><Button style={{marginLeft: 5}} small transparent onPress={() => Speech.speak(props.indicator)}><Icon name="volume-high"/></Button></Left>
-            <Right><Button transparent onPress={() => Alert.alert(props.indicator, "Some more information", )}><Text>Learn More</Text><Icon name="information-circle-outline"/></Button></Right>
+            <Left><Text>{props.indicator}</Text><Button style={{marginLeft: 5}} small transparent onPress={() => Speech.speak(props.indicator, {language})}><Icon name="volume-high"/></Button></Left>
+            <Right><Button transparent onPress={() => Alert.alert(props.indicator, "Some more information", )}><Text>{ui.learnMore[language]}</Text><Icon name="information-circle-outline"/></Button></Right>
         </CardItem>
         <CardItem style={{flexDirection: 'column'}}>
-            {props.questions.map((question, index) => <Question key={index} question={question}/>)}
+            {questions.map((question, index) => <Question language={language} key={index} question={question}/>)}
         </CardItem>
     </Card>
 }
 
-const Questionnaire: React.FC<{ assessments: AssessmentProps[] }> = props => {
-    const {assessments} = props;
+const Questionnaire: React.FC = () => {
+    const [language, setLanguage] = useState('en');
 
     return <ScrollView style={{padding: 5}}>
-        {assessments.map(assessment => <Assessment key={assessment.indicator} indicator={assessment.indicator} questions={assessment.questions}/>)}
+        <Card style={{flex: 0, margin: 8}}>
+            <CardItem header>
+                <Text>{ui.changeLanguage[language]}</Text>
+            </CardItem>
+            <CardItem><Picker
+                note
+                mode="dropdown"
+                style={{ width: 120 }}
+                selectedValue={language}
+                onValueChange={setLanguage}
+            >
+                <Picker.Item label="English" value="en" />
+                <Picker.Item label="Deutsch" value="de" />
+                <Picker.Item label="Francais" value="fr" />
+            </Picker></CardItem>
+        </Card>
+        {questions[language].map(assessment => <Assessment language={language} key={assessment.indicator} indicator={assessment.indicator} questions={assessment.questions}/>)}
     </ScrollView>
 }
 
 const QuestionScreen: React.FC = () => {
-    return <Questionnaire assessments={questions}/>
+    return <Questionnaire/>
 }
 
 export {QuestionScreen}
