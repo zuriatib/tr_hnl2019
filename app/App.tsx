cc50ef76fca2fe85f8fdfee5f261fe9bc86a0143
Lukas Lücke <lukas@luecke.me>
2019-11-03 10:38:54 +0100

    Setup react-navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,16 @@
+import {NavigationNativeContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack";
 import React from 'react';
 import {HomeScreen} from "./components/HomeScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-      <HomeScreen/>
+      <NavigationNativeContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{title: "Home"}}/>
+        </Stack.Navigator>
+      </NavigationNativeContainer>
   );
 }
